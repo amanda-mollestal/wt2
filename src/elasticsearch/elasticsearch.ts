d50d431359576c94fe2169abecfe16248b713215
Amanda Möllestål <am224pg@student.lnu.se>
2023-05-09 23:26:17 +0200

    fix vercel bug

--- src/elasticsearch/elasticsearch.ts
+++ src/elasticsearch/elasticsearch.ts
@@ -1,7 +1,7 @@
 import { Client } from '@elastic/elasticsearch'
-import dotenv from 'dotenv'
+//import dotenv from 'dotenv'
 
-dotenv.config()
+//dotenv.config()
 const elasticId = process.env.ELASTIC_CLOUD_ID
 const elasticUser = process.env.ELASTIC_CLOUD_USERNAME
 const elasticPassword = process.env.ELASTIC_CLOUD_PASSWORD
